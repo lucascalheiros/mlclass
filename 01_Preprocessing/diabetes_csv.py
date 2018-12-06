4e907a33705de5a6dd446f4352948c3c10ed113a
Dayvson Sales <dayvson@outlook.com>
2018-12-05 22:41:00 -0300

    primeiras modificacoe

--- 01_Preprocessing/diabetes_csv.py
+++ 01_Preprocessing/diabetes_csv.py
@@ -1,37 +1,66 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Atividade para trabalhar o pré-processamento dos dados.
+Created on Wed Dec  5 21:51:52 2018
 
-Criação de modelo preditivo para diabetes e envio para verificação de peformance
-no servidor.
-
-@author: Aydano Machado <aydano.machado@gmail.com>
+@author: dayvsonsales
 """
-
+import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import requests
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.metrics import accuracy_score
+
 print('\n - Lendo o arquivo com o dataset sobre diabetes')
-data = pd.read_csv('diabetes_dataset.csv')
+data = pd.read_csv('/Users/dayvsonsales/mlclass/01_Preprocessing/diabetes_dataset.csv')
+
+zero = ['Glucose','BloodPressure','SkinThickness','BMI','Insulin']
+
+for col in zero:
+    data[col]= data[col].replace(0, np.NaN)
+    mean = int(data[col].mean(skipna=True))
+    data[col] = data[col].replace(np.NaN, mean)
 
 # Criando X and y par ao algorítmo de aprendizagem de máquina.\
 print(' - Criando X e y para o algoritmo de aprendizagem a partir do arquivo diabetes_dataset')
+
 # Caso queira modificar as colunas consideradas basta algera o array a seguir.
 feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
-                'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+
 X = data[feature_cols]
 y = data.Outcome
 
-# Ciando o modelo preditivo para a base trabalhada
+#se colocar test_size = 0.0001 ele dá 100% mas fica viciado
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.02, random_state = 1)
+
+#há varios escalares esse pega o maior absoluto
+sc = MaxAbsScaler()
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Criando o modelo preditivo para a base trabalhada
 print(' - Criando modelo preditivo')
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(X, y)
+neigh = KNeighborsClassifier(n_neighbors = 3)
+neigh.fit(X_train, y_train)
 
 #realizando previsões com o arquivo de
 print(' - Aplicando modelo e enviando para o servidor')
-data_app = pd.read_csv('diabetes_app.csv')
+
+#data_app = pd.read_csv('/Users/dayvsonsales/mlclass/01_Preprocessing/diabetes_app.csv')
+#y_pred = neigh.predict(data_app)
+y_pred = neigh.predict(X_test)
+
+print(accuracy_score(y_test,y_pred))
+
+data_app = pd.read_csv('/Users/dayvsonsales/mlclass/01_Preprocessing/diabetes_app.csv')
+##se mudar os atributos, tem que mudar aqui
+#data_app = data_app[feature_cols]
+data_app = sc.transform(data_app)
 y_pred = neigh.predict(data_app)
 
 # Enviando previsões realizadas com o modelo para o servidor
@@ -41,12 +70,11 @@ URL = "https://aydanomachado.com/mlclass/01_Preprocessing.php"
 DEV_KEY = "COLOCAR_SUA_KEY_AQUI"
 
 # json para ser enviado para o servidor
-data = {'dev_key':DEV_KEY,
+data = {'dev_key':'if\'s',
         'predictions':pd.Series(y_pred).to_json(orient='values')}
 
 # Enviando requisição e salvando o objeto resposta
-r = requests.post(url = URL, data = data)
+#r = requests.post(url = URL, data = data)
 
 # Extraindo e imprimindo o texto da resposta
-pastebin_url = r.text
-print(" - Resposta do servidor:\n", r.text, "\n")
+#print(" - Resposta do servidor:\n", r.text, "\n")
